64f1d443bfd886fe715e8c1ebf53d208a73fe758
Lyrklif <kochurov121@gmail.com>
2019-08-30 05:52:14 +0400

    вёрстка

---
+++ assets/src/js/_includes/people-say-slider.js
@@ -0,0 +1,21 @@
+// Слайдер отзывов в блоке People say
+// $('.people-say__list').slick({
+//     arrows: false,
+//     dots: true,
+//     appendDots: '.people-say__dots-wp',
+//     dotsClass: 'people-say__dots userpic-dots'
+
+// });
+
+
+$('.people-say__list').slick({
+    arrows: false,
+    // dots: true,
+    // appendDots: $('.people-say__dots-wp'),
+    // dotsClass: 'people-say__dots userpic-dots'
+
+});
+
+
+
+
--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -5,11 +5,13 @@
 
 // Подключение бибилиотек
 @@include('assets/src/js/_libs/jquery.js') // jquery 3.3.1
+@@include('assets/src/js/_libs/slick.min.js') // slick slider
 //('assets/src/js/_libs/jquery.maskedinput.min.js') // Маска для input
 
 
 
 // Мой код
 $(document).ready(function() {
+    @@include('assets/src/js/_includes/people-say-slider.js') 
 
 });
